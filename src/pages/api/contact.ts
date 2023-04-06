9dab11c7848ec2eeadc18618f8fc636af5836c64
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-06 16:11:36 -0600

    Added backend api route for message creation

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,24 @@
+import { Message } from '@/models/message';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method, body } = req;
+  const { email, name, message } = body;
+
+  switch (method) {
+    case 'POST':
+      const newMessage: Message = {
+        email,
+        name,
+        message,
+      };
+      console.log(newMessage);
+      res.status(201).json(newMessage);
+      break;
+    default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed!`);
+  }
+}
+
+export default handler;
